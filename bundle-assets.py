23f39befa7faad4d642976a2e43370dd2d258f51
Osmo Salomaa <otsaloma@iki.fi>
2022-09-04 04:05:11 +0300

    Refactor

--- bundle-assets.py
+++ bundle-assets.py
@@ -13,30 +13,23 @@ ASSET_PATTERNS = {
 
 def read_asset(directory, asset):
     if asset.startswith("https://"):
-        return read_url(asset)
-    return read_file(directory, asset)
-
-def read_file(directory, fname):
-    print("READ: {}".format(fname))
-    path = directory / fname
+        print("GET: {}".format(asset))
+        response = requests.get(asset)
+        response.raise_for_status()
+        return response.text
+    print("READ: {}".format(asset))
+    path = directory / asset
     return path.read_text("utf-8").strip()
-
-def read_url(url):
-    print("GET: {}".format(url))
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
 
 for path in map(Path, sys.argv[1:]):
     print(f"Processing {path!s}...")
-    directory = path.parent
     lines = path.read_text("utf-8").splitlines()
     for i, line in enumerate(lines):
         for pattern, replacement in ASSET_PATTERNS.items():
             match = re.search(pattern, line)
             if match is None: continue
             asset = match.group(1).split("?")[0]
-            content = read_asset(directory, asset)
+            content = read_asset(path.parent, asset)
             content = replacement.format(content)
             a, z = match.span()
             lines[i] = line[:a] + content + line[z:]
