9163f41221e51fa6baede8c0d40b48b19e296798
Osmo Salomaa <otsaloma@iki.fi>
2022-09-10 01:54:45 +0300

    Use 33% and 67% quantiles

--- main.js
+++ main.js
@@ -6,23 +6,24 @@ function addClasses(div, title, pastDay, pastHour) {
     pastHour && div.classList.add("past-hour");
 }
 
-function appendBar(chart, width, median, time, price, title, pastDay, pastHour) {
+function appendBar(chart, width, q33, q67, time, price, title, pastDay, pastHour) {
     // Bar
     let div = document.createElement("div");
     div.classList.add("bar");
     addClasses(div, title, pastDay, pastHour);
-    console.log(median);
-    div.style.backgroundImage = [
-        `linear-gradient(
-             to right,
-             transparent 0 calc(${median}% - 1px),
-             var(--color-ref) calc(${median}% - 1px) calc(${median}% + 1px),
-             transparent calc(${median}% + 1px) 100%)`,
-        `linear-gradient(
-             to right,
-             var(--color-bar) 0 ${width}%,
-             transparent ${width}% 100%)`,
-    ].join(", ");
+    div.style.backgroundImage = `
+        linear-gradient(
+            to right,
+            transparent 0 calc(${q33}% - 1px),
+            var(--color-ref) calc(${q33}% - 1px) calc(${q33}% + 1px),
+            transparent calc(${q33}% + 1px) calc(${q67}% - 1px),
+            var(--color-ref) calc(${q67}% - 1px) calc(${q67}% + 1px),
+            transparent calc(${q67}% + 1px) 100%
+        ), linear-gradient(
+            to right,
+            var(--color-bar) 0 ${width}%,
+            transparent ${width}% 100%
+        )`;
     chart.appendChild(div);
     // Time label
     div = document.createElement("div");
@@ -68,36 +69,43 @@ function isPastHour(date, now) {
     return (date.getTime() + 3600*1000) < now.getTime();
 }
 
-function median(values) {
+function quantile(values, q) {
     values.sort((a, b) => a - b);
-    const i = Math.floor(values.length / 2);
-    if (values.length % 2 === 1)
-        return values[i];
-    return (values[i-1] + values[i]) / 2;
+    const pos = (values.length - 1) * q;
+    const i = Math.floor(pos);
+    const diff = pos - i;
+    return values[i] * (1 - diff) + values[i+1] * diff;
 }
 
 function renderChart(data) {
     if (data.length === 0) return;
     const now = new Date();
     const ref = getReferenceData(data);
+    const refPrices = ref.map(x => x.price_with_vat);
     const chart = document.getElementById("chart");
     const priceMax = 1.05 * Math.max(...data.map(x => x.price_with_vat));
-    const priceMedian = median(ref.map(x => x.price_with_vat));
+    const priceQ33 = quantile(refPrices, 0.33);
+    const priceQ67 = quantile(refPrices, 0.67);
     for (const item of data) {
         // Append hourly bars to chart along with time and price labels.
-        const width  = Math.round(item.price_with_vat / priceMax * 100);
-        const median = Math.round(priceMedian / priceMax * 100);
+        const width = Math.round(item.price_with_vat / priceMax * 100);
+        const q33 = Math.round(priceQ33 / priceMax * 100);
+        const q67 = Math.round(priceQ67 / priceMax * 100);
         const title = item.time === "00:00";
-        const time  = title ? `${formatDate(item.date)}` : item.time;
-        const price = title ? `${formatPrice(item.price_with_vat)} snt/kWh` : formatPrice(item.price_with_vat);
+        const time = title ? `${formatDate(item.date)}` : item.time;
+        const price = formatPrice(item.price_with_vat);
         const datetime = new Date(item.datetime.replace(" ", "T"));
-        const pastDay  = isPastDay(datetime, now);
+        const pastDay = isPastDay(datetime, now);
         const pastHour = isPastHour(datetime, now);
-        appendBar(chart, width, median, time, price, title, pastDay, pastHour);
+        appendBar(chart, width, q33, q67, time, price, title, pastDay, pastHour);
     }
-    for (const span of document.getElementsByClassName("tick median")) {
-        span.innerHTML = `${formatPrice(priceMedian)} snt/kWh`;
-        span.style.paddingLeft = `calc(${(priceMedian / priceMax * 100).toFixed(1)}% - 0.5em)`;
+    for (const span of document.getElementsByClassName("tick q33")) {
+        span.innerHTML = `${formatPrice(priceQ33)}`;
+        span.style.paddingLeft = `calc(${(priceQ33 / priceMax * 100).toFixed(1)}% - 0.5em)`;
+    }
+    for (const span of document.getElementsByClassName("tick q67")) {
+        span.innerHTML = `${formatPrice(priceQ67)} snt/kWh`;
+        span.style.paddingLeft = `calc(${(priceQ67 / priceMax * 100).toFixed(1)}% - 0.5em)`;
     }
 }
 
@@ -105,5 +113,4 @@ function renderChart(data) {
     fetch("prices.json")
         .then(response => response.json())
         .then(data => renderChart(data));
-
 })();
